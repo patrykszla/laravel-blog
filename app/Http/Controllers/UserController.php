46b68bbf88c92125b1c7f8d3effa7b52f4a6480b
Patryk Szlachta <patryk.szla@interia.pl>
2023-07-25 20:17:08 +0200

    work with user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -11,9 +12,11 @@ class UserController extends Controller
     public function register(Request $request) {
         // var_dump($request);
         $incomingFields = $request->validate([
-            'username' => 'required',
-            'email' => 'required',
-            'password' => 'required',
+            // Class Rule unique - first table in database second column
+            'username' => ['required', 'min:3', 'max:20', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            //laravel looking for password_confirmation and check is the same as password
+            'password' => ['required', 'min:5', 'confirmed'],
         ]);
         User::create($incomingFields);
         // var_dump($incomingFields);
