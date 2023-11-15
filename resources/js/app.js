6e55b3d40dcbf9bc210525283c7822d36f478fa8
Patryk Szlachta <patryk.szla@interia.pl>
2023-11-15 21:51:35 +0100

    work on broadcasting -problem with axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
 import './bootstrap';
 import Search from './live-search';
+import Chat from "./chat";
 
 if(document.querySelector(".header-search-icon")) {
     new Search();
 }
 // alert('test 123444');
+
+if(document.querySelector(".header-chat-icon")) {
+    new Chat();
+}
